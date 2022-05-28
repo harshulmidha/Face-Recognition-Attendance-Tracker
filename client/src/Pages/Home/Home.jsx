4ed132c5f83b6db6e57cce8c35ef6a9bf826099a
harshulmidha <harshulmidha@gmail.com>
2022-05-28 20:58:19 +0530

    Adding youtube link

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -6,9 +6,10 @@ const Home = () => {
             <h1 id='AT'><center>Face Recognition - ATTENDANCE TRACKER</center></h1>
             <br/><br/><br/>
             <h2>Choose an option from the top right to start your journey!</h2><br/>
-            <h2><b>Student:</b> Login using your device Camera/Webcam</h2><br/>
-            <h2><b>Teacher:</b> Login using a password to access Attendance Records</h2><br/><br/><br/>
-            <h3>Note: If you are logging in as a student, you are required to give the camera access permission to the website.</h3>
+            <h2>Student: Login using your device Camera/Webcam</h2><br/>
+            <h2>Teacher: Login using a password to access Attendance Records</h2><br/><br/><br/>
+            <h3>Link to Video Demo: <a id="demo" href="https://youtu.be/2mbSjsMOrkM">https://youtu.be/2mbSjsMOrkM</a></h3><br/><br/><br/>
+            <h4>Note: If you are logging in as a student, you are required to give the camera access permission to the website.</h4>
         </div>
     )
 }
